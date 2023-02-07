44d7e8fd467a7de431f48d21a66718219143745d
Cole Ferrer <chorhanglam@gmail.com>
2023-02-07 17:16:36 -0500

    basic impl

--- appPlay/playMain.js
+++ appPlay/playMain.js
@@ -1,7 +1,25 @@
+import * as banner from '../esModules/ephemeral-banner/index.js';
+import { NoteOnEvt } from '../esModules/midi-data/midiEvent.js';
+import * as sound from '../esModules/musical-sound/musicalSound.js';
+import * as pubSub from '../esModules/pub-sub/pubSub.js';
+import * as math from '../esModules/math-util/mathUtil.js';
 
 main();
 
 function main() {
+  const [soundPub, soundSub] = pubSub.make();
+  const [metronomeBeatPub, metronomeBeatSub] = pubSub.make();
+  const [readyPub, readySub] = pubSub.make();
+  const [playEndedPub, playEndedSub] = pubSub.make();
+  const [currTimePub, currTimeSub] = pubSub.make();
+
+  const eBanner = banner.setup();
+  const musicalSound = new sound.MusicalSound({
+    midiJs: window.MIDI, soundSub: soundSub, 
+    eBanner: eBanner, readyPub: readyPub
+  });
+
+
   let beatsPerBar = 4;
   let numSubdivisions = 1;
   let currBeat = 0;
@@ -35,6 +53,7 @@ function main() {
           playingIntervalId = null;
           return;
       }
+
       restart();
   }
   function restart() {
@@ -42,16 +61,38 @@ function main() {
         clearInterval(playingIntervalId);
         playingIntervalId = null;
       }
+  musicalSound.configure([{
+      channelNum: 0,
+      instrumentName: sound.instruments.synth_drum,
+    }]);
       playingIntervalId = window.setInterval(_ => {
-          let utterance = new SpeechSynthesisUtterance(`${currBeat + 1}`);
-          utterance.rate = 0.9;
-          window.speechSynthesis.speak(utterance);
+          // let utterance = new SpeechSynthesisUtterance(`${currBeat + 1}`);
+          // utterance.rate = 0.9;
+          // window.speechSynthesis.speak(utterance);
+        getEvts(currBeat, beatsPerBar).forEach(evt => {
+          evt.channelNum = 0;
+          musicalSound.execute(new NoteOnEvt(evt));
+        });
+        // musicalSound.execute(new NoteOnEvt({
+        //   noteNum: 36,
+        //   velocity: 80,
+        //   channelNum: 0,
+        // }));
           currBeat += 1;
           currBeat = currBeat % beatsPerBar;
       }, 60 / beatsPerMin * 1000);
   }
 }
 
-function createCycle(beatsPerBar, numSubdivisions) {
-
+function getEvts(idx, numBeats) {
+    if (idx == 0) {
+      return [{noteNum: 36, velocity: 90}, {noteNum: 42, velocity: 30}];
+    }
+    if (idx == numBeats - 1) {
+      return [{noteNum: 37, velocity: 30}, {noteNum: 42, velocity: 30}];
+    }
+    if (math.mod(idx, 2) == 0) {
+      return [{noteNum: 36, velocity: 30}, {noteNum: 42, velocity: 30}];
+    }
+    return [{noteNum: 36, velocity: 30}, {noteNum: 44, velocity: 60}];
 }
